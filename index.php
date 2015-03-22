95e05f544b3030f9d5a00014a94e644170265e55
ArthurHlt <arthurh.halet@gmail.com>
2015-03-22 11:18:57 +0100

    sensiolab fix

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $phpSelf = $_SERVER['PHP_SELF'];
 if (basename($phpSelf) === "index.php") {
     header('Location: app/app.php');
-    die(1);
+    return 1;
 }
 return require_once 'vendor/autoload.php';
